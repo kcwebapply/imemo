a4e3019fe35719c9be73e08061fb34853ef9ec96
kcaiueo <kcwebapply@gmail.com>
2019-06-23 00:48:07 +0900

    imple clustring view

--- view/view-temp.go
+++ view/view-temp.go
@@ -15,14 +15,32 @@ func PrintAllMemo(memos []model.Memo) {
 	for _, memo := range memos {
 		printBody(memo)
 	}
+
 	fmt.Println(dotLine + "\x1b[0m")
+}
+
+func PrintAllMemos(memoMapByCategory map[int][]model.Memo, categoryIDMap map[int]model.Category) {
+
+	// loop for each category
+	for categoryID, memolist := range memoMapByCategory {
+		category := categoryIDMap[categoryID]
+
+		fmt.Println(category.Name)
+		fmt.Println(dotLine)
+		for _, memo := range memolist {
+			printBody(memo)
+		}
+		fmt.Println(dotLine + "\x1b[0m")
+
+	}
+
 }
 
 // PrintSaveMessage is function of printing message when saving memo.
 func PrintAddMemo(memo model.Memo) {
 	//fmt.Println("\x1b[1;94mmemo saved!\x1b[0m")
-	fmt.Printf("x1b[38;5;39m > %s\n", memo.Memo)
-	fmt.Println("\x1b[1m\x1b[38;5;39mmemo saved!\x1b[0m")
+	fmt.Printf("\x1b[38;5;39m>> %s\n", memo.Memo)
+	fmt.Println("\x1b[0mmemo saved!")
 }
 
 func printBody(memo model.Memo) {
