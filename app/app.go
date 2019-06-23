5a11704f80cada4a201a59d909a1fbc8b75bc56c
kcaiueo <kcwebapply@gmail.com>
2019-06-23 23:40:28 +0900

    delete unused class

--- app/app.go
+++ app/app.go
@@ -1,12 +1,8 @@
 package app
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/codegangsta/cli"
 	"github.com/kcwebapply/imemo/domain/service/commands"
-	validator "github.com/kcwebapply/imemo/validator"
 )
 
 var appName = "imemo"
@@ -30,11 +26,6 @@ func InitApp() *cli.App {
 	app.Commands = commands.GetCommandMap()
 	// command validator registration
 	app.Before = func(c *cli.Context) error {
-		err := validator.ValidateArgument(c)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(0)
-		}
 		return nil
 	}
 
