d6144dacd0de5fae9a38d0dce5087a39e563b69c
kcaiueo <kcwebapply@gmail.com>
2019-06-30 13:46:19 +0900

    feature/sqlite-sequence-refactoring

--- domain/repository/category_repository.go
+++ domain/repository/category_repository.go
@@ -4,45 +4,50 @@ import (
 	"github.com/kcwebapply/imemo/domain/model"
 )
 
+const (
+	CATEGORY = "category"
+)
+
 func CreateCategoryTable() {
 	_, _ = sess.Exec("create table category(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT);")
 }
 
 func InsertDefaultCategory() {
 	var category = model.Category{ID: 1, Name: "default"}
-	sess.InsertInto("category").Columns("id", "name").Record(category).Exec()
+	sess.InsertInto(CATEGORY).Columns("id", "name").Record(category).Exec()
 
 }
 
 // GetCategories return all memodata.
 func GetCategories() ([]model.Category, error) {
 	var rows []model.Category
-	_, err := sess.Select("*").From("category").Load(&rows)
+	_, err := sess.Select("*").From(CATEGORY).Load(&rows)
 	return rows, err
 }
 
 // GetCategories return all memodata.
 func GetCategoryByName(name string) (model.Category, error) {
 	var rows model.Category
-	_, err := sess.Select("*").From("category").Where("name = ?", name).Load(&rows)
+	_, err := sess.Select("*").From(CATEGORY).Where("name = ?", name).Load(&rows)
 	return rows, err
 }
 
 // AddCategory add category entity.
 func AddCategory(category model.Category) error {
 
-	_, err := sess.InsertInto("category").Columns("name").Record(category).Exec()
+	_, err := sess.InsertInto(CATEGORY).Columns("name").Record(category).Exec()
 	return err
 }
 
 // DeleteCategory
 func DeleteCategoryById(id int) error {
-	_, err := sess.DeleteFrom("category").Where("id = ?", id).Exec()
+	_, err := sess.DeleteFrom(CATEGORY).Where("id = ?", id).Exec()
 	return err
 }
 
 // DeleteCategory
 func DeleteAllCategory() error {
-	_, err := sess.DeleteFrom("category").Exec()
+	_, err := sess.DeleteFrom(CATEGORY).Exec()
+	_, err = sess.DeleteFrom("sqlite_sequence").Where("name = ?", CATEGORY).Exec()
 	return err
 }
